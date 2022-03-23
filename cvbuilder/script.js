27bad31f72bd89ec75206bf5288e8961fd3aff4e
nurdica21 <selimovic.nurdin982@gmail.com>
2022-03-23 20:52:15 +0100

    resume builder

---
+++ cvbuilder/script.js
@@ -0,0 +1,7 @@
+function next(){
+
+    document.querySelector("#form1").style.display = "none";
+    document.querySelector("#form2").style.display = "flex";
+    document.querySelector("h1").innerHTML="Step 2";
+   
+}
